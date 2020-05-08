a50211af8717c38c9f1539f8fbf0e133af373109
PWolf <pwolf@aviareps.com>
2020-05-08 19:27:42 +0200

    node blog page started

---
+++ www/sites/default/files/php/twig/5eb3d0c6d3e0e_views-view-field--team-ov_7mNKqXuQVszxQOsWJflWF0nti/oW15_t_4Ki0ai_neWrCGetondJ7CyHMaXSKHbrV0-1Q.php
@@ -0,0 +1,88 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* sites/all/themes/perks/templates/views/team-view/views-view-field--team-overview.html.twig */
+class __TwigTemplate_c08b502e72107ce0f0546ca6d332b03840ba0ca20688fd91ec7d1a5c7e496ba8 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 1];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["output"] ?? null)), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "sites/all/themes/perks/templates/views/team-view/views-view-field--team-overview.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{{ output }}
+", "sites/all/themes/perks/templates/views/team-view/views-view-field--team-overview.html.twig", "/var/www/drupalvm/drupal/web/perks-employee-website/www/sites/all/themes/perks/templates/views/team-view/views-view-field--team-overview.html.twig");
+    }
+}
---
+++ www/sites/default/files/php/twig/5eb3d0c6d3e0e_views-view-unformatted--s_CjvUVXbLFh0KL_7fshiDGyrWV/YkJwWNpuOJNwu2GcHiNSYYOrz1y_zVyIW97QwGSD_mk.php
@@ -0,0 +1,123 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* sites/all/themes/perks/templates/views/section-one-view/views-view-unformatted--section-content.html.twig */
+class __TwigTemplate_936f50eb4d7820b50f743b9e4a41c21adbd9dfd5e667a839c439c4408f58ed5a extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 20];
+        $filters = ["escape" => 22];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 20
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["rows"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["row"]) {
+            // line 21
+            echo "<div class=\"row align-items-center\">
+    ";
+            // line 22
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["row"], "content", [])), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['row'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "sites/all/themes/perks/templates/views/section-one-view/views-view-unformatted--section-content.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 22,  59 => 21,  55 => 20,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+* @file
+* Default theme implementation to display a view of unformatted rows.
+*
+* Available variables:
+* - title: The title of this group of rows. May be empty.
+* - rows: A list of the view's row items.
+*   - attributes: The row's HTML attributes.
+*   - content: The row's content.
+* - view: The view object.
+* - default_row_class: A flag indicating whether default classes should be
+*   used on rows.
+*
+* @see template_preprocess_views_view_unformatted()
+*
+* @ingroup themeable
+*/
+#}
+{% for row in rows %}
+<div class=\"row align-items-center\">
+    {{ row.content }}
+  </div>
+{% endfor %}
+", "sites/all/themes/perks/templates/views/section-one-view/views-view-unformatted--section-content.html.twig", "/var/www/drupalvm/drupal/web/perks-employee-website/www/sites/all/themes/perks/templates/views/section-one-view/views-view-unformatted--section-content.html.twig");
+    }
+}
